9e9f7ce3775acbba3bf1aae9025187603606c68a
Malcolm Harrow <malcolm.harrow@gmail.com>
2015-01-24 12:12:56 +0800

    Initial Commit

---
+++ TestThreads/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestThreads
+//
+//  Created by Malcolm on 24/1/15.
+//  Copyright (c) 2015 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestThreads/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestThreads
+//
+//  Created by Malcolm on 24/1/15.
+//  Copyright (c) 2015 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
